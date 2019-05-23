6d86fac02fab99a6542317f84f89689de073fb21
Félix Darraud <felix.darraud@gmail.com>
2019-05-23 17:30:18 +0200

    Changed loofDocument to singular

--- app/LoofDocument.php
+++ app/LoofDocument.php
@@ -4,9 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class LoofDocuments extends Model
+class LoofDocument extends Model
 {
     protected $table = 'Loof_documents';
     protected $primaryKey = 'loof_document_id';
     protected $visible = ['loof_document_url'];
+
 }
