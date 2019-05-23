c0757db01b424796b245344e0d038bbeaf5bf3a7
Félix Darraud <felix.darraud@gmail.com>
2019-05-23 17:13:27 +0200

    Add breed model & migration

---
+++ app/Breed.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Breed extends Model
+{
+    protected $table = 'Breeds';
+    protected $primaryKey = 'breed_id';
+    protected $visible = ['cat_api_breed_id'];
+}
