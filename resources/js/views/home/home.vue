27dfa728c4be78239bf2a102179b024f2603fa00
Pauline DUVAL <pauline.duval27@outlook.fr>
2019-05-26 19:33:58 +0200

    begin HomePage vue and scss

--- resources/js/views/home/home.vue
+++ resources/js/views/home/home.vue
@@ -1,8 +1,17 @@
+
 <template>
-    <div id="home">
-        HomePage
-        <router-link to='test'>Link to test</router-link>
-    </div>
+  <div id="home">
+<div id="header">
+<h1> kitCat </h1>
+</div>
+    <ul id="container-cats">
+      <li class="cat-bloc"  v-for="cat in cats">
+        <img class="cat-img" :src="cat.min">
+        <h2>{{ cat.name }}</h2>
+
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script lang="text/javascript" src="./home.js"></script>
