4c25060760bf9be55763c7a8145b062bffaf1c32
Pauline DUVAL <pauline.duval27@outlook.fr>
2019-05-28 23:09:37 +0200

    CatsFromApi created

--- resources/js/views/home/home.vue
+++ resources/js/views/home/home.vue
@@ -1,17 +1,10 @@
 
 <template>
   <div id="home">
-<div id="header">
-</div>
-<div id="header-title">
-<h1> kitCat </h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dolor libero, congue quis lacus vel, sodales sollicitudin ipsum.</p>
-</div>
     <ul id="container-cats">
       <li class="cat-bloc"  v-for="cat in cats">
         <img class="cat-img" :src="cat.min">
-        <h2>{{ cat.name }}</h2>
-
+        <h2>{{ cat.cat_name }}</h2>
       </li>
     </ul>
   </div>
